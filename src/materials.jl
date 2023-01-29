92e757eec6eff0271ab6ddd5771d2008267a9cb9
Bartek Chaber <bartosz.chaber@pw.edu.pl>
2023-01-29 23:18:37 +0100

    Move PML higher

--- src/materials.jl
+++ src/materials.jl
@@ -7,6 +7,11 @@ struct Conductor end
 struct Dielectric{EPSILON, MU, SIGMA} <: Medium{EPSILON, MU, SIGMA} 
   id :: UInt8
 end
+struct PerfectlyMatchedLayer{EPSILON, MU} <: Medium{EPSILON, MU, 0.0}
+  id :: UInt8
+   σ :: Float64
+   m :: Int64
+end
 
 import Base: convert
 convert(::Type{UInt8}, d::Dielectric) = d.id
@@ -16,10 +21,4 @@ Metal  = Conductor()
 Vacuum = Dielectric{ε_0, μ_0, 0.0}(0xff)
 PTFE   = Dielectric{2.04ε_0, μ_0, 0.0}(0xfe)
 Air    = Dielectric{1.0006ε_0, μ_0, 0.0}(0xae)
-
-struct PerfectlyMatchedLayer{EPSILON, MU} <: Medium{EPSILON, MU, 0.0} 
-  id :: UInt8
-   σ :: Float64
-   m :: Int64
-end
 end
