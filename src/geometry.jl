9f706edde85892b6cd3c38a104f7af9de1257555
lodygaw <wiklod.spam@gmail.com>
2023-02-16 16:12:48 +0100

    Add Polygon geometry - added Polygon <: Shape type - added intersect((x,y),::Polygon) - added in((x,y), ::Segment) - added 2D cross and dot products (to avoid dependencies) - added basic tests for both in() and intersect()

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,7 +1,6 @@
 module Geometry
 export Rectangle, Circle, Segment, Polygon
-import Base: +, -, ∈
-import LinearAlgebra: norm, ⋅
+import Base: +, -, ∈, ∩
 
 const mm = 1e-3 # unit conversion ratio [mm/m]
 const cm = 1e-2 # unit conversion ratio [cm/m]
@@ -16,10 +15,6 @@ struct CompositeShape{OPERATOR, A, B}  <: Shape end
 struct Polygon{N} <: Shape
 	segments :: NTuple{N, Segment}
 end
-struct Ray
-	origin :: NTuple{2, Float64}
-	direction :: NTuple{2, Float64}
-end
 
 function Polygon(nodes::Vector{NTuple{2, Float64}})
 	segments = Vector{Segment}()
@@ -29,41 +24,59 @@ function Polygon(nodes::Vector{NTuple{2, Float64}})
 	push!(segments, Segment{nodes[end]..., nodes[1]...}())
 	return Polygon{length(segments)}(Tuple(segments))
 end
+
+struct Ray
+	origin :: NTuple{2, Float64}
+	direction :: NTuple{2, Float64}
+end
 	
 function ∈((x, y), ::Rectangle{X, Y, W, H}) where {X, Y, W, H}
-	if X <= x <= X + W
-		if Y <= y <= Y + H
-			return true
-		end
+	if (X <= x <= X + W) && (Y <= y <= Y + H)
+		return true
 	end
-
 	return false
 end
 
 function ∈((x, y), ::Circle{X, Y, R}) where {X, Y, R}
 	r² = (x - X)^2 + (y - Y)^2
 	if r² <= R^2
-	return true
+		return true
 	end
-
 	return false
 end
 
+function ∈((x, y), ::Segment{X1, Y1, X2, Y2}) where {X1, Y1, X2, Y2}
+	d₁ = sqrt((x - X1)^2 + (y - Y1)^2)
+	d₂ = sqrt((x - X2)^2 + (y - Y2)^2)
+	d₃ = sqrt((X1 - X2)^2 + (Y1 - Y2)^2)
+	return d₁ + d₂ ≈ d₃
+end
+
 function ∈((x, y), polygon::Polygon{N}) where {N}
-	θ = 2π * rand() 
-	ray = Ray((x,y),(sin(θ),cos(θ)))
+
+	if any(s->((x,y) ∈ s), polygon.segments)
+		return true
+	end
+
+	# θ = 2π * rand() 
+	# d_x = sin(θ)/(sqrt(sin(θ)^2 + cos(θ)^2))
+	# d_y = cos(θ)/(sqrt(sin(θ)^2 + cos(θ)^2))
+	# ray = Ray((x,y),(d_x, d_y))
+
+	ray = Ray((x,y),(1., 0.))
+
 	n_crossings = 0
 	for segment in polygon.segments
 		if ray ∩ segment
 			n_crossings += 1
 		end
 	end
-	@info "$n_crossings"
-	@info "$ray"
+
 	return (n_crossings % 2) == 1
 end
 
-×(v1::Tuple{Float64,Float64}, v2::Tuple{Float64,Float64}) = v1[1] * v2[2] - v1[2] * v2[1]
+×(v₁::Tuple{Float64,Float64}, v₂::Tuple{Float64,Float64}) = v₁[1] * v₂[2] - v₁[2] * v₂[1]
+⋅(v₁::Tuple{Float64,Float64}, v₂::Tuple{Float64,Float64}) = v₁[1] * v₂[1] + v₁[2] * v₂[2]
 
 function ∩(ray::Ray, segment::Segment{X1, Y1, X2, Y2}) where {X1, Y1, X2, Y2}
 	a = (X1, Y1)
@@ -73,8 +86,12 @@ function ∩(ray::Ray, segment::Segment{X1, Y1, X2, Y2}) where {X1, Y1, X2, Y2}
 	v₂ = b .- a
 	v₃ = ray.direction .* (-1, 1)
 
-	t₁ = norm(v₂ × v₁) ./ (v₂ ⋅ v₃)
-	t₂ = (v₁ ⋅ v₃) ./ (v₂ ⋅ v₃)
+	v₂v₃ = v₂ ⋅ v₃
+	
+	if v₂v₃ ≈ 0.0 return false end
+
+	t₁ = (v₂ × v₁) / (v₂v₃)
+	t₂ = (v₁ ⋅ v₃) / (v₂v₃)
 
 	return (t₁ >= 0) && (0 <= t₂ <= 1)
 end
---
+++ test/runtests.jl
@@ -0,0 +1,48 @@
+using Test
+
+@testset "Geometry" begin
+
+import PlasmaModelingToolkit.Geometry: Polygon, Segment, Ray, ∩, ∈
+
+p1 = Polygon([(0.,0.), (4.,0.), (4.,4.), (0.,4.)])
+p2 = Polygon([(-4.,-4.), (-2.,-4.), (-2.,-2.), (-4.,-2.)])
+
+@testset "Polygon" begin
+	@test (2.,2.) ∈ p1 				# inside
+	@test (2.,0.) ∈ p1				# on the side (bottom)
+	@test (4.,2.) ∈ p1				# on the side (right)
+	@test (2.,4.) ∈ p1				# on the side (top)
+	@test (0.,2.) ∈ p1				# on the side (left)
+	@test (0.,0.) ∈ p1				# on the corner (left-bottom, origin)
+	@test (4.,0.) ∈ p1				# on the corner (right-bottom)
+	@test (4.,4.) ∈ p1				# on the corner (right-top)
+	@test (0.,4.) ∈ p1				# on the corner (left-top)
+	@test !((5.,5.) ∈ p1)			# outside
+	@test !((-5.,2.) ∈ p1)			# outside
+	@test !((-5.,-5.) ∈ p1)			# outside
+	@test !((-1e-5,-1e-5) ∈ p1)		# outside
+	@test (-3.,-3.) ∈ p2 			# inside
+	@test (-3.,-4.) ∈ p2			# on the side (bottom)
+	@test (-2.,-3.) ∈ p2			# on the side (right)
+	@test (-3.,-2.) ∈ p2			# on the side (top)
+	@test (-4.,-3.) ∈ p2			# on the side (left)
+	@test (-4.,-4.) ∈ p2			# on the corner (left-bottom)
+	@test (-2.,-4.) ∈ p2			# on the corner (right-bottom)
+	@test (-2.,-2.) ∈ p2			# on the corner (right-top)
+	@test (-4.,-2.) ∈ p2			# on the corner (left-top)
+	@test !((5.,5.) ∈ p2)			# outside
+	@test !((-5.,2.) ∈ p2)			# outside
+	@test !((-5.,-5.) ∈ p2)			# outside
+	@test !((1e-5,1e-5) ∈ p2)		# outside
+end
+
+s1 = Segment{0.,0.,0.,4.}()
+r1 = Ray((-2.,2.),(1.,0.))
+r2 = Ray((2.,2.),(-1.,0.))
+
+@testset "Ray-Segment intersection" begin
+	@test r1 ∩ s1
+	@test r2 ∩ s1
+end
+
+end
