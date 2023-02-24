0fbba425a831e3addc21fe1b1b8bce16d7fc8034
Bartek Chaber <bartosz.chaber@pw.edu.pl>
2023-02-24 20:08:58 +0100

    PerfectlyMatchedLayer should wrap Dielectric

--- src/materials.jl
+++ src/materials.jl
@@ -8,16 +8,17 @@ struct Conductor <: Material end
 struct Dielectric{EPSILON, MU, SIGMA} <: Medium{EPSILON, MU, SIGMA} 
   id :: UInt8
 end
-struct PerfectlyMatchedLayer{EPSILON, MU} <: Medium{EPSILON, MU, 0.0}
-  id :: UInt8
-   σ :: Float64
-   m :: Int64
+struct PerfectlyMatchedLayer{EPSILON, MU, 0.0} <: Medium{EPSILON, MU}
+  dielectric :: Dielectric{EPSILON, MU, 0.0}
+  σ :: Float64
+  m :: Int64
 end
 
 import Base: convert
-convert(::Type{UInt8}, m::PerfectlyMatchedLayer) = m.id
 convert(::Type{UInt8}, d::Dielectric) = d.id
 convert(::Type{UInt8}, c::Conductor)  = 0x00
+convert(::Type{UInt8}, p::PerfectlyMatchedLayer) where T =
+  convert(UInt8, p.dielectric)
 
 Metal()  = Conductor()
 Vacuum() = Dielectric{ε_0, μ_0, 0.0}(0xff)
