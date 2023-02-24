9008dcd7a2dac2e6a7a0ab1384d18716ab313c9b
Bartek Chaber <bartosz.chaber@pw.edu.pl>
2023-02-24 20:08:14 +0100

    Fix unit names

--- src/units.jl
+++ src/units.jl
@@ -2,7 +2,7 @@ module Units
 const cm = 1e-2 # (mm/m)
 const mm = 1e-3 # (mm/m)
 const nm = 1e-9 # (mm/m)
-const kHZ = 1e3 # (kHz/Hz)
-const MHZ = 1e6 # (kHz/Hz)
-const GHZ = 1e9 # (kHz/Hz)
+const kHz = 1e3 # (kHz/Hz)
+const MHz = 1e6 # (kHz/Hz)
+const GHz = 1e9 # (kHz/Hz)
 end
