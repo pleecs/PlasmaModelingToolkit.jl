583f20c77519072727335b5d81681e6ebe1ce010
Wiktor Łodyga <wiktor.lodyga.dokt@pw.edu.pl>
2023-03-29 16:47:06 +0200

    Update swo.jl example, bump version - add boundary conditions - add time-varying CoaxialPort and SurfaceImpedance - workaround ring SVG rendering issue - TODO: adjust temporal parameters - TODO: add antenna

--- examples/swo.jl
+++ examples/swo.jl
@@ -1,23 +1,28 @@
 RADIUS = 0.05             # radius along r-axis [m]
 LENGTH = 0.1925           # lenght along z-axis [m]
+D_RADIUS = 0.5            # domain lenght along z-axis [m]
+D_LENGTH = 0.5            # domain lenght along z-axis [m]
 
 import PlasmaModelingToolkit.Domains: AxisymmetricDomain
 import PlasmaModelingToolkit.Geometry: Rectangle, Circle, Segment, mm, Polygon
 import PlasmaModelingToolkit.Constants: ε_0, μ_0, η_0
 import PlasmaModelingToolkit.Materials: Air, Metal, PerfectlyMatchedLayer, PTFE, Vacuum
 import PlasmaModelingToolkit.BoundaryConditions: SurfaceImpedance, PerfectElectricConductor, PerfectMagneticConductor
+import PlasmaModelingToolkit.Sources: CoaxialPort, AperiodicSignal
+import PlasmaModelingToolkit.TemporalFunctions: GeneralizedLogisticFunction 
 import PlasmaModelingToolkit.SVG: Figure, save, svg
 
-domain = AxisymmetricDomain(0.5, 0.5, Air())
+domain = AxisymmetricDomain(D_LENGTH, D_RADIUS, Air())
 
 outer 	= Rectangle{0, 0, 171mm, RADIUS}()
 outer  -= Rectangle{30mm, 0, 141mm, 48mm}()
 outer  -= Rectangle{5mm, 0, 25mm, 23mm}()
 outer  -= Circle{30mm, 23mm, 25mm}()
-outer  += Circle{166mm, RADIUS-2mm, 10mm}()
-outer  -= Rectangle{156mm, RADIUS-12mm, 20mm, 10mm}()
-outer  -= Rectangle{171mm, RADIUS-2mm, 5mm, 10mm}()
-outer  -= Rectangle{156mm, RADIUS, 5mm, 10mm}()
+ring    = Circle{166mm, RADIUS-2mm, 10mm}()
+ring   -= Rectangle{156mm, RADIUS-12mm, 20mm, 10mm}()
+ring   -= Rectangle{171mm, RADIUS-2mm, 5mm, 10mm}()
+ring   -= Rectangle{156mm, RADIUS-2mm, 5mm, 10mm}()
+outer  += ring
 
 dielec  = Rectangle{5mm, 18mm, 170.5mm, 45.5mm}()
 dielec -= Rectangle{9mm, 18mm, 21mm, 5mm}()
@@ -32,9 +37,24 @@ inner += Rectangle{30mm, 0mm, 150.5mm, 44mm}()
 inner += Circle{180.5mm, 39.5mm, 4.5mm}()
 inner += Polygon([(180.5mm, 0mm), (192.5mm, 0mm), (192.5mm, 10mm), (180.5mm, 22mm)])
 
+PML_th = 0.01
+
+axis   = Segment{D_LENGTH, 0.0, 192.5mm, 0.0}()
+input  = Segment{171mm, 48mm, 171mm, 44mm}()
+sparkgap   = Segment{9mm, 0.0, 5mm, 0.0}()
+top_pml    = Rectangle{D_LENGTH - PML_th, 0mm, PML_th, D_RADIUS}()
+side_pml   = Rectangle{0mm, D_RADIUS - PML_th, D_LENGTH, PML_th}()
+bottom_pml = Rectangle{0mm, RADIUS, PML_th, D_RADIUS - RADIUS}()
+
 domain[dielec] = PTFE()
-domain[outer] = Metal()
-domain[inner] = Metal()
+domain[outer]  = Metal()
+domain[inner]  = Metal()
+domain[axis]   = PerfectMagneticConductor()
+domain[input]  = CoaxialPort(AperiodicSignal{GeneralizedLogisticFunction(0.0, 1.0, 1e-9, 1e5)}(), 2.04ε_0) # FIXME: adjust GLF parameters
+domain[sparkgap]   = SurfaceImpedance(GeneralizedLogisticFunction(η_0, 1e-2, 20e-9, 1e5), ε_0) # FIXME: adjust GLF parameters
+domain[top_pml]	   = PerfectlyMatchedLayer(Air(), 0.7(0.02/π), 2)
+domain[side_pml]   = PerfectlyMatchedLayer(Air(), 0.7(0.02/π), 2)
+domain[bottom_pml] = PerfectlyMatchedLayer(Air(), 0.7(0.02/π), 2)
 
 f = Figure(domain; width=25)
 f.margin 			= 1
