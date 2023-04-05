d7dfc1aa2e5a928ca22f80b21e2873e749c6777d
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2023-04-05 10:39:36 +0200

    Add Dirichlet and Neumann boundary condition

--- src/boundaries.jl
+++ src/boundaries.jl
@@ -2,7 +2,9 @@ module BoundaryConditions
 import ..TemporalFunctions: TemporalFunction
 
 abstract type BoundaryCondition end
+struct NeumannBoundaryCondition  <: BoundaryCondition end
 struct PeriodicBoundaryCondition <: BoundaryCondition end
+struct DirichletBoundaryCondition<: BoundaryCondition end
 struct PerfectElectricConductor  <: BoundaryCondition end
 struct PerfectMagneticConductor  <: BoundaryCondition end
 mutable struct SurfaceImpedance  <: BoundaryCondition
