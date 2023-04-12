691f98944b3fbbc9a024dc82a4d89a36e9bf7122
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2023-04-12 17:38:39 +0200

    Fix import errors with preliminary version of FDMModel

--- src/PlasmaModelingToolkit.jl
+++ src/PlasmaModelingToolkit.jl
@@ -1,14 +1,14 @@
 module PlasmaModelingToolkit
+include("grid.jl")
 include("constants.jl")
 include("temporal.jl")
 include("materials.jl")
 include("geometry.jl")
-include("models.jl")
 include("boundaries.jl")
 include("domains.jl")
 include("species.jl")
 include("sources.jl")
+include("models.jl")
 include("units.jl")
 include("plots.jl")
-include("grid.jl")
 end
