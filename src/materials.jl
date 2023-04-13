785fe2d8cc8cc45919a5b16b22dfe7243ddd9517
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2023-04-13 12:34:32 +0200

    Remove dielectric's id, create map material=>UInt8, boundary=>UInt8

--- src/materials.jl
+++ src/materials.jl
@@ -5,22 +5,15 @@ abstract type Material end
 abstract type Medium{EPSILON, MU, SIGMA} <: Material end
 
 struct Conductor <: Material end
-struct Dielectric{EPSILON, MU, SIGMA} <: Medium{EPSILON, MU, SIGMA} 
-  id :: UInt8
-end
+struct Dielectric{EPSILON, MU, SIGMA} <: Medium{EPSILON, MU, SIGMA} end
 struct PerfectlyMatchedLayer{EPSILON, MU, SIGMA} <: Medium{EPSILON, MU, SIGMA}
   dielectric :: Dielectric{EPSILON, MU, SIGMA}
   σ :: Float64
   m :: Int64
 end
 
-import Base: convert
-convert(::Type{UInt8}, d::Dielectric) = d.id
-convert(::Type{UInt8}, c::Conductor)  = 0x00
-convert(::Type{UInt8}, p::PerfectlyMatchedLayer) = 0x80 + (p.dielectric.id)
-
 Metal()  = Conductor()
-Vacuum() = Dielectric{ε_0, μ_0, 0.0}(0x01)
-PTFE()   = Dielectric{2.04ε_0, μ_0, 0.0}(0x02)
-Air()    = Dielectric{1.0006ε_0, μ_0, 0.0}(0x03)
+Vacuum() = Dielectric{ε_0, μ_0, 0.0}()
+PTFE()   = Dielectric{2.04ε_0, μ_0, 0.0}()
+Air()    = Dielectric{1.0006ε_0, μ_0, 0.0}()
 end
