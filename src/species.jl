bdf501bcf9a6b7e9bb0b17cf5797285efe4edfe7
Wiktor Łodyga <wiktor.lodyga.dokt@pw.edu.pl>
2023-04-26 15:47:32 +0200

    Refactor Species module - KineticSpecies are now Particles - FluidSpecies are now Fluid - create Particles and Fluid based on Atom (ions, particles, gas functions) - define electrons()

--- src/species.jl
+++ src/species.jl
@@ -1,6 +1,11 @@
 module Species
-export KineticSpecies
+import ..PlasmaModelingToolkit.Atoms: Atom
 
-struct KineticSpecies{D, V} end
-struct FluidSpecies end
+struct Particles{SYM, Q, M} end
+struct Fluid{SYM, M} end
+
+electrons() = Particles{:e, -q_e, m_e}()
+ions(atom::Atom, n = 1) = Particles{Symbol("i"^n * String(atom.sym)), (n * q_e), atom.mass - (n * m_e)}()
+particles(atom::Atom) = Particles{atom.sym, 0.0, atom.mass}()
+gas(atom::Atom) = Fluid{atom.sym, atom.mass}()
 end
