72c4dcc4a0666446d25b2268ad1f1fbdb78034b9
Wiktor Łodyga <wiktor.lodyga.dokt@pw.edu.pl>
2023-04-27 15:05:52 +0200

    Replace abstract Model with more specific Problems - add ParticleProblem - refactor BoundaryValueProblem

--- src/problems.jl
+++ src/problems.jl
@@ -1,16 +1,35 @@
 module Problems
+import ..Geometry: Rectangle
+import ..Boundaries: ParticleBoundary
+import ..BoundaryConditions: BoundaryCondition
+import ..Sources: SpeciesSource, SpeciesLoader
 import Base: setindex!
 
-struct ParticleProblem
+struct ParticleProblem{D}
+	domain :: D
+	boundaries :: Vector{Pair{Any, ParticleBoundary}}
+	sources :: Vector{Pair{Rectangle, SpeciesSource}}
+	loaders :: Vector{Pair{Rectangle, SpeciesLoader}} 
+	ParticleProblem(domain) = new{typeof(domain)}(domain, [], [], [])
+end
+
+function setindex!(problem::ParticleProblem, phenomenon, region)
+	if phenomenon isa ParticleBoundary
+    	push!(model.boundaries, region => phenomenon)
+    elseif phenomenon isa SpeciesSource
+    	push!(model.sources, region => phenomenon)
+	elseif phenomenon isa SpeciesLoader
+		push!(model.loaders, region => phenomenon)
+	end
 end
 
 struct BoundaryValueProblem{D}
 	domain :: D
-	constraints :: Vector{Pair{Any, Any}}
+	constraints :: Vector{Pair{Any, BoundaryCondition}}
 	BoundaryValueProblem(domain) = new{typeof(domain)}(domain, [])
 end
 
-function setindex!(model::BoundaryValueProblem, constraint, region)
-    push!(model.constraints, region => constraint)
+function setindex!(problem::BoundaryValueProblem, constraint::BoundaryCondition, region)
+    push!(problem.constraints, region => constraint)
 end
 end
