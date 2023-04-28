5bcb5ed0abe9276007228146da747f0027668f0c
Wiktor Łodyga <wiktor.lodyga.dokt@pw.edu.pl>
2023-04-28 15:41:41 +0200

    PICModel examples(twostreams, turner2013-case-1) update, MCCModel (first ideas)

--- examples/twostreams.jl
+++ examples/twostreams.jl
@@ -38,29 +38,26 @@ lower = Segment{0.0, 0.0, 0.0, R}()
 upper = Segment{Z, R, Z, 0.0}()
 whole = Rectangle{0.0, 0.0, Z, R}()
 
-problem = ParticleProblem{2,3}(domain)
-problem[side] = ReflectingBoundary()
-problem[lower] = PeriodicBoundary()			#assumes its a full edge of a domain 
-problem[upper] = PeriodicBoundary()			 #assumes its a full edge of a domain 
-
-
 bvp = BoundaryValueProblem(domain)
 bvp[axis] = NeumannBoundaryCondition()
 bvp[side] = NeumannBoundaryCondition()
 bvp[upper] = PeriodicBoundaryCondition()			#assumes its a full edge of a domain 
 bvp[lower] = PeriodicBoundaryCondition()			#assumes its a full edge of a domain 
 
+problem = ParticleProblem{2,3}(domain)
+problem[side] = ReflectingBoundary()
+problem[lower] = PeriodicBoundary()					#assumes its a full edge of a domain 
+problem[upper] = PeriodicBoundary()			 		#assumes its a full edge of a domain 
+
 e   = electrons()
 iHe = ions(Helium)
 
-# ν_drift = (z, r, θ)
-problem[whole] = SpeciesLoader(e, 0.5n_0, UniformDistribution(), MaxwellBoltzmannDistribution{T_e, e.mass}(), drift = (+ν_drift, 0.0, 0.0))
-problem[whole] = SpeciesLoader(e, 0.5n_0, UniformDistribution(), MaxwellBoltzmannDistribution{T_e, e.mass}(), drift = (-ν_drift, 0.0, 0.0))
-
+problem[whole] = SpeciesLoader(e, 0.5n_0, UniformDistribution(), MaxwellBoltzmannDistribution{T_e, e.mass}(), drift = (+ν_drift, 0.0, 0.0)) # ν_drift = (z, r, θ)
+problem[whole] = SpeciesLoader(e, 0.5n_0, UniformDistribution(), MaxwellBoltzmannDistribution{T_e, e.mass}(), drift = (-ν_drift, 0.0, 0.0)) # ν_drift = (z, r, θ)
 problem[whole] = SpeciesLoader(iHe, n_0, UniformDistribution(), MaxwellBoltzmannDistribution{T_i, iHe.mass}())
 
 es  = FDMModel(bvp, NZ + 1, NR + 1)
 pic = PICModel(problem, NZ + 1, NR + 1, Δt = 5, weight = (e => WG, iHe => WG), maxcount = (e => 20_000, iHe => 20_000))
 
 # Hennel.j;
-solver = Hennel.Solvers.create(pic, es)
+# solver = Hennel.Solvers.create(pic, es)
