3ccc50dd60a1aae1c0259533f35a88c72a47d3bb
Bartosz Chaber <bartosz.chaber@pw.edu.pl>
2023-05-12 08:58:51 +0200

    Add time units and fix comments

--- src/units.jl
+++ src/units.jl
@@ -1,8 +1,11 @@
 module Units
-const cm = 1e-2 # (mm/m)
+const ms = 1e-3 # (ms/s)
+const ns = 1e-9 # (ns/s)
+const ps = 1e-12# (ps/s)
+const cm = 1e-2 # (cm/m)
 const mm = 1e-3 # (mm/m)
-const nm = 1e-9 # (mm/m)
+const nm = 1e-9 # (nm/m)
 const kHz = 1e3 # (kHz/Hz)
-const MHz = 1e6 # (kHz/Hz)
-const GHz = 1e9 # (kHz/Hz)
+const MHz = 1e6 # (MHz/Hz)
+const GHz = 1e9 # (GHz/Hz)
 end
