196acfdbfad7905fcc09907d947a677882fc250c
Wiktor Łodyga <wiktor.lodyga.dokt@pw.edu.pl>
2023-05-18 17:08:13 +0200

    Make PICModel and MCCModel dimension independent - unify style of FDTD and FDM condition unions

--- src/models/pic.jl
+++ src/models/pic.jl
@@ -8,8 +8,10 @@ boundaries :: Vector{Pair{Shape{D}, ParticleBoundary}}
    loaders :: Vector{Pair{Shape{D}, SpeciesLoader}} 
 end
 
-function PICModel(problem::ParticleProblem{2,:ZR}, nz, nr; maxcount, weights)
-  grid = discretize(problem.domain, nz, nr)
+function PICModel(problem::ParticleProblem{D,CS}, args...; maxcount, weights) where {D,CS}
+  @assert length(args) == D
+
+  grid = discretize(problem.domain, args...)
   particles = Set{Particles}()
   weights = Dict(weights...)
   maxcount = Dict(maxcount...)
@@ -29,41 +31,10 @@ function PICModel(problem::ParticleProblem{2,:ZR}, nz, nr; maxcount, weights)
     end
   end
 
-  return PICModel{2,3,:ZR}(grid, particles, weights, maxcount, boundaries, sources, loaders)
+  return PICModel{D,3,CS}(grid, particles, weights, maxcount, boundaries, sources, loaders)
 end
 
-function PICModel(problem::ParticleCollisionProblem{2,:ZR}, nz, nr; maxcount, weights)
-  return PICModel(problem.particles, nz, nr, maxcount=maxcount, weights=weights)
-end
-
-function PICModel(problem::ParticleProblem{1, :X}, nx; maxcount, weights)
-  grid = discretize(problem.domain, nx)
-  particles = Set{Particles}()
-  weights = Dict(weights...)
-  maxcount = Dict(maxcount...)
-  boundaries = problem.boundaries
-  sources = problem.sources
-  loaders = problem.loaders
-
-  for (_, source) in sources
-    if source.species isa Particles
-      push!(particles, source.species)
-    end
-  end
-
-  for (_, loader) in loaders
-    if loader.species isa Particles
-      push!(particles, loader.species)
-    end
-  end
-  return PICModel{1,3,:X}(grid, particles, weights, maxcount, boundaries, sources, loaders)
-end
-
-function PICModel(problem::ParticleCollisionProblem{1,:X}, nx; maxcount, weights)
-  return PICModel(problem.particles, nx, maxcount=maxcount, weights=weights)
-end
-
-function setindex!(model::PICModel, bc::ParticleBoundary, point::Point1D)
-  # TODO: add this
-  return nothing
+function PICModel(problem::ParticleCollisionProblem{D,CS}, args...; maxcount, weights) where{D,CS}
+  @assert length(args) == D
+  return PICModel(problem.particles, args..., maxcount=maxcount, weights=weights)
 end
