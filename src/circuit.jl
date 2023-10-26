8731dc10cb2d7c666bb0bfa7f133d3e1f4f61a07
bchaber <bartosz.chaber@pw.edu.pl>
2023-10-26 13:47:38 +0200

    Add structures for lumped ports.

--- src/circuit.jl
+++ src/circuit.jl
@@ -12,4 +12,12 @@ Inductor(L) = LumpedElement(:L, L)
 Capacitor(C) = LumpedElement(:C, C)
 VoltageSource(V) = LumpedElement(:V, V)
 
+struct LumpedPort{CIR, EXC}
+	circuit :: CIR
+	terminals :: Tuple{Symbol, Symbol}
+	excitation :: EXC
+end
+LumpedPort(circuit; terminals::Tuple{Symbol, Symbol},
+	excitation=nothing) = LumpedPort(circuit, terminals, excitation)
+
 end
