b6fbd18b37fb7dab87753aff2986d38a9689967e
bchaber <bartosz.chaber@pw.edu.pl>
2023-12-22 08:14:36 +0100

    Add some missing units

--- src/units.jl
+++ src/units.jl
@@ -1,15 +1,27 @@
 module Units
 const ms = 1e-3 # (ms/s)
+const μs = 1e-6 # (μs/s)
 const ns = 1e-9 # (ns/s)
 const ps = 1e-12# (ps/s)
 const cm = 1e-2 # (cm/m)
 const mm = 1e-3 # (mm/m)
 const nm = 1e-9 # (nm/m)
+const Hz = 1e0 # frequency unit
 const kHz = 1e3 # (kHz/Hz)
 const MHz = 1e6 # (MHz/Hz)
 const GHz = 1e9 # (GHz/Hz)
-
+const Ω   = 1e0  # resistance unit
+const mΩ  = 1e-3 # (mΩ/Ω)
+const kΩ  = 1e+3 # (kΩ/Ω)
+const MΩ  = 1e+6 # (MΩ/Ω)
+const V   = 1e0 # voltage unit
+const kV  = 1e3 # (kV/V)
+const MV  = 1e6 # (MV/V)
+const A   = 1e0  # current unit
+const mA  = 1e-3 # (mA/A)
+const kA  = 1e+3 # (kA/A)
 const Td  = 1e-21 # Townsend (V * m^2)
+const Pa  = 1e0 # pressure unit
 const atm = 101_325 # standard atmosferic pressure (Pa)
 const torr = 1atm / 760 # torr (Pa)
 end
